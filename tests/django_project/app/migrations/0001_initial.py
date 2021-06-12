16db7e7ad1a18c12e5b871ab1a1751140cd3f34e
Josh <josh.doncastermarsiglio@tophatmonocle.com>
2021-06-12 14:54:37 -0400

    fix migration dependency

--- tests/django_project/app/migrations/0001_initial.py
+++ tests/django_project/app/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("auth", "0012_alter_user_first_name_max_length"),
+        ("auth", "0010_alter_group_name_max_length"),
     ]
 
     operations = [
